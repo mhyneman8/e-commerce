95395322e3fec8949310de614a43990cf6a50ad5
mhyneman8 <mallory.hyneman@gmail.com>
2021-11-27 16:12:19 -0600

    implement authentication

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Login from './components/Login';
 import ProductList from './components/ProductList';
 
 import Context from './Context';
+import axios from 'axios';
+import jwt_decode from 'jwt-decode';
 
 // const routerRef = React.forwardRef((props, ref) => {
 //   return <input ref={ref}/>;
@@ -22,6 +24,42 @@ export default class App extends Component {
     };
     this.routerRef = React.createRef();
   }
+
+  componentDidMount() {
+    let user = localStorage.getItem('user');
+    user = user ? JSON.parse(user) : null;
+    this.setState({ user });
+  }
+
+  login = async (email, password) => {
+    const res = await axios.post(
+      'http://localhost:3001/login',
+      { email, password },
+    ).catch((res) => {
+      return { status: 401, message: 'Unauthorized' }
+    })
+
+    if( res.status === 200) {
+      const { email } = jwt_decode(res.data.accessToken)
+      const user = {
+        email,
+        token: res.data.accessToken,
+        accessLevel: email === 'admin@example.com' ? 0 : 1
+      }
+
+      this.setState({ user });
+      localStorage.setItem('user', JSON.stringify(user));
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  logout = e => {
+    e.preventDefault();
+    this.setState({ user: null });
+    localStorage.removeItem('user');
+  };
 
   render() {
     return (
